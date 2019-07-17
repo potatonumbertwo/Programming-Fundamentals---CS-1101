a8b6af7722ee4703647ee54311a07dbe3dfa1ba7
potatonumbertwo <lsj900605@gmail.com>
2019-07-17 15:01:09 -0400

    6.4 is working but need to use recursion to replace %

--- unit_4_assignment.py
+++ unit_4_assignment.py
@@ -2,24 +2,17 @@ def is_power(a, b):
     """ define a function that a is a power of b if a is divisible by b
     and a/b is a power of b ,returns True if a is a power of b"""
 
-    if a == b:
+    if a == b:  # base case of two arguments are equal
         return True
     if b == 1:
-        return False
-    isADivisibleByB = is_divisible(a, b) == 1
-    isADiviedByBIsPowerofB = a / b % b == 0
+        return False  # base case of the second argument is 1
+    isADivisibleByB = is_divisible(a, b) == 1  # assign is_divisible() to a variable to check is_divisible is True
+    isADiviedByBIsPowerofB = a / b % b == 0  # variable checks a/b is power of b ,== mean equal here
     if isADivisibleByB and isADiviedByBIsPowerofB:
-        return is_power(a / b, b)
-    #
+        return is_power(a / b, b)  # call itself recursively
+
     else:
         return False
-
-
-# print(is_divisible(10, 2))
-# print(is_divisible(27, 3))
-# print(is_divisible(1, 1))
-# print(is_divisible(10, 1))
-# print(is_divisible(3, 3))
 
 
 def is_divisible(x, y):
@@ -34,4 +27,3 @@ print("is_power(27, 3) returns: ", is_power(27, 3))
 print("is_power(1, 1) returns: ", is_power(1, 1))
 print("is_power(10, 1) returns: ", is_power(10, 1))
 print("is_power(3, 3) returns: ", is_power(3, 3))
-

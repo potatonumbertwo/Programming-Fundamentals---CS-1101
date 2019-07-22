cba74d548bbefe3faf215095224b2369511c0d33
potatonumbertwo <lsj900605@gmail.com>
2019-07-22 17:15:09 -0400

    Discussion

--- unit_4_assignment.py
+++ unit_4_assignment.py
@@ -1,6 +1,9 @@
+import time
+start_time = time.time()
+
 def is_power(a, b):
-    """ define a function that a is a power of b if a is divisible by b
-    and a/b is a power of b ,returns True if a is a power of b"""
+    # """ define a function that a is a power of b if a is divisible by b
+    # and a/b is a power of b ,returns True if a is a power of b"""
 
     if a == b:  # base case of two arguments are equal
         return True
@@ -9,7 +12,7 @@ def is_power(a, b):
     isADivisibleByB = is_divisible(a, b) == 1  # assign is_divisible() to a variable to check is_divisible is True
     isADiviedByBIsPowerofB = a / b % b == 0  # variable checks a/b is power of b ,== mean equal here
     if isADivisibleByB and isADiviedByBIsPowerofB:
-        return is_power(a / b, b)  # call itself recursively
+        return True  # call itself recursively
 
     else:
         return False
@@ -27,3 +30,15 @@ print("is_power(27, 3) returns: ", is_power(27, 3))
 print("is_power(1, 1) returns: ", is_power(1, 1))
 print("is_power(10, 1) returns: ", is_power(10, 1))
 print("is_power(3, 3) returns: ", is_power(3, 3))
+print(time.time() - start_time)
+
+
+
+# print(6.508827209472656e-05 - 7.486343383789062e-05)
+
+# is_power(10, 2) returns:  False
+# is_power(27, 3) returns:  True
+# is_power(1, 1) returns:  True
+# is_power(10, 1) returns:  False
+# is_power(3, 3) returns:  True
+# 7.295608520507812e-05
---
+++ unit_5_discussion_8_4.py
@@ -0,0 +1,114 @@
+def any_lowercase1(s):
+    for c in s:
+        if c.islower():
+            return True
+        else:
+            return False
+
+
+print(any_lowercase1('Potato'))
+print(any_lowercase1('poTato'))
+'''
+ The function check if c is lowercase ,if c is lowercase it returns True.
+if c is not lowercase it returns False.
+print(any_lowercase1('Potato')),The output return False.
+Because the function body is using for loop,if the first letter
+isn't lowercase it will return False ,and then end the loop.
+
+print(any_lowercase1('poTato')) output is True 
+
+print(any_lowercase1('poTato')) returns True because the first letter is lowercase,
+it will keep checking,it will stop until it isn't lowercase.
+'''
+
+
+def any_lowercase2(s):
+    for c in s:
+        if 'c'.islower():
+            print('c')
+            return 'True'
+        else:
+            return 'False'
+
+
+print(any_lowercase2('Potato'))
+
+'''
+This function is check if 'c' is lowercase.And it will always turns string True.
+Because it won't check the c in s,it only checks 'c'
+if we change the function like this,
+'''
+
+
+def any_lowercase2(s):
+    for c in s:
+        if 'C'.islower():
+            print('c')
+            return 'True'
+        else:
+            return 'False'
+
+
+print(any_lowercase2('Potato'))
+
+'''
+it will turn string False ,because 'C' is uppercase.
+
+'''
+
+
+def any_lowercase3(s):
+    for c in s:
+        flag = c.islower()
+    return flag
+
+
+print(any_lowercase3('Potato'))
+
+'''
+This function checks any lowercase in s,if it is not lowercase it will keep running
+until all letter from s to be checked. it returns True if it has at least one lowercase.
+This example returns True because there are 5 lowercase letters.
+'''
+
+
+def any_lowercase4(s):
+    flag = False
+    for c in s:
+        flag = flag or c.islower()
+    return flag
+
+
+print(any_lowercase4('Potato'))
+print(any_lowercase4('POTATO'))
+
+'''
+any_lowercase4 function is check if c in s has any lowercase.If c is lowercase,
+then return False or True ,and keep running until all letter from c to be checked.
+If there is any lowercase it will return True.
+print(any_lowercase4('POTATO')) will return False because there hasn't any lowercase.
+
+'''
+
+
+def any_lowercase5(s):
+    for c in s:
+        if not c.islower():
+            return False
+
+    return True
+
+
+print(any_lowercase5('Potato'))
+print(any_lowercase5('potato'))
+
+'''
+any_loercase5 function checks the c.islower is True or False. It's check all letter in s hast to be lowercase .
+If there is any uppercase it will return False
+
+print(any_lowercase5('Potato')) return False, Because the first letter 'P' is not lowercase so return False
+directly and then end this program.
+
+However , print(any_lowercase5('potato')) will return True which means if it's lowercase,will keep running 
+until find any not lowercase and then end the program 
+'''

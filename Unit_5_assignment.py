f56c12f825d716f013d090a86ac033d59fc056fe
potatonumbertwo <lsj900605@gmail.com>
2019-07-23 10:56:45 -0400

    Add docstring ,format is still hard to write

--- Unit_5_assignment.py
+++ Unit_5_assignment.py
@@ -2,6 +2,8 @@ import math
 
 
 def my_sqrt(a):
+    """ define a function named my_sqrt,take a as a parameter,
+    using while loop to calculate square root,and returns the square root"""
     x = 10
 
     while True:
@@ -12,6 +14,8 @@ def my_sqrt(a):
 
 
 def test_sqrt():
+    """define a function named test_sqrt, using my_sqrt function
+    to compare with math.sqrt,print out their difference"""
     for i in range(1, 26):
         mysqrt = my_sqrt(i)
 
@@ -19,10 +23,8 @@ def test_sqrt():
 
         diff = abs(math_sqrt - float(mysqrt))
 
-        # print('a = ' + str(i) + ' | ' + 'my_sqrt(' + str(i) + ') = ' + str(my_sqrt(i)) + ' | ' + 'math.sqrt(' + str(
-        #     i) + ') = ' + str(math.sqrt(i)) + ' | ' 'diff = ' + str(diff))
-
-        # print('{}, {}, {},{}'.format(a=i, my_sqrt=mysqrt, math_sqrt=math_sqrt, diff=diff))
+        print('a = ' + str(i) + ' | ' + 'my_sqrt(' + str(i) + ') = ' + str(my_sqrt(i)) + ' | ' + 'math.sqrt(' + str(
+            i) + ') = ' + str(math.sqrt(i)) + ' | ' 'diff = ' + str(diff))
 
 
 test_sqrt()

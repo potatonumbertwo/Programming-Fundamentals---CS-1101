c4b9b4d2452d08c6d03d9e86cd8a3d0c0917533d
potatonumbertwo <lsj900605@gmail.com>
2019-07-23 07:55:18 -0400

    Trys to use format to print table

---
+++ Unit_5_assignment.py
@@ -0,0 +1,26 @@
+import math
+
+
+def my_sqrt(a):
+    x = 10
+
+    while True:
+        y = (x + a / x) / 2
+        if y == x:
+            return y
+        x = y
+
+
+def test_sqrt():
+    for i in range(1, 26):
+        mysqrt = my_sqrt(i)
+
+        math_sqrt = math.sqrt(i)
+
+        diff = abs(math_sqrt - float(mysqrt))
+
+        print('a = ' + str(i) + ' | ' + 'my_sqrt(' + str(i) + ') = ' + str(my_sqrt(i)) + ' | ' + 'math.sqrt(' + str(
+            i) + ') = ' + str(math.sqrt(i)) + ' | ' 'diff = ' + str(diff))
+
+
+test_sqrt()

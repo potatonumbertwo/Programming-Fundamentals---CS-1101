2678c5058ca2170e7ac8658b57805fdca25b4709
potatonumbertwo <lsj900605@gmail.com>
2019-08-10 16:12:55 -0400

    out put is working

--- Unit_7/unit7_learning Journal.py
+++ Unit_7/unit7_learning Journal.py
@@ -1,7 +1,16 @@
 # created a dictionary with list as values
 stocks = {'AMD': [28.26, 32.02], 'AAPL': [196.48, 200.00],
-          'FB': [184.68, 190.00], 'AMAZ': [1786.00, 1800.00],
-          'SPOT': [149.00, 156.35], 'DlS': [136.78, 130.34]}
+          'FB': [200.00, 190.00], 'AMAZ': [1786.00, 1800.00],
+          'SPOT': [149.00, 200.00], 'DlS': [136.78, 130.34]}
+
+id = {'Director': ['Stanley Kubrick', 'Ridley Scott', 'James Cameron', 'Quentin Tarantino', 'Christopher Nolan',
+                   'Alejandro Gonzalez Inarritu', 'Clint Eastwood', 'Mel Gibson', 'Steven Spielberg'],
+      'Writer': ['Stanley Kubrick', 'Steven Spielberg', 'Christopher Nolan', 'Ethan Coen', 'Oliver Stone'],
+      'Producer': ['Steven Spielberg', 'Clint Eastwood', 'Joel Coen', 'Ridley Scott', 'James Cameron',
+                   'Stanley Kubrick'],
+      'Actor': ['Al Pacino', 'Robert De Niro', 'Tom Hanks', 'Clint Eastwood', 'Mel Gibson', 'Quentin Tarantino',
+                'Leonardo DiCaprio', 'Morgan Freeman'],
+      'DocumentaryNarrator': ['Morgan Freeman', 'Tom Hanks', 'David Attenborough', 'James Earl Jones']}
 
 
 def invert_dict(dictionary):
@@ -17,22 +26,10 @@ def invert_dict(dictionary):
 
 print(invert_dict(stocks))
 
+# file1 = open("myfile.txt","w")
+import json
 
+with open("myfile.json", "w") as fp:
+    json.dump(stocks, fp)
 
-# {28.26: 'AMD', 32.02: 'AMD', 196.48: 'AAPL', 200.0: 'AAPL',
-# 184.68: 'FB', 190.0: 'FB', 1786.0: 'AMAZ', 1800.0: 'AMAZ',
-# 149.0: 'SPOT', 156.35: 'SPOT', 136.78: 'DlS', 130.34: 'DlS'}
-
-
-# def invert_dict(d):
-#     inverse = dict()
-#     for key in d:
-#         val = d[key]
-#         if val not in inverse:
-#             inverse[val] = [key]
-#         else:
-#             inverse[val].append(key)
-#     return inverse
-#
-#
-# print(invert_dict(stacks))
+# file= open()

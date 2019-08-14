3179cfe0fcbfe439ec1498d6e27a85a02afd5dfd
potatonumbertwo <lsj900605@gmail.com>
2019-08-13 22:11:21 -0400

    Error examples

--- Unit_7/unit7_learning Journal.py
+++ Unit_7/unit7_learning Journal.py
@@ -1,16 +1,10 @@
+import pickle
+import json
+
 # created a dictionary with list as values
 stocks = {'AMD': [28.26, 32.02], 'AAPL': [196.48, 200.00],
           'FB': [200.00, 190.00], 'AMAZ': [1786.00, 1800.00],
           'SPOT': [149.00, 200.00], 'DlS': [136.78, 130.34]}
-
-id = {'Director': ['Stanley Kubrick', 'Ridley Scott', 'James Cameron', 'Quentin Tarantino', 'Christopher Nolan',
-                   'Alejandro Gonzalez Inarritu', 'Clint Eastwood', 'Mel Gibson', 'Steven Spielberg'],
-      'Writer': ['Stanley Kubrick', 'Steven Spielberg', 'Christopher Nolan', 'Ethan Coen', 'Oliver Stone'],
-      'Producer': ['Steven Spielberg', 'Clint Eastwood', 'Joel Coen', 'Ridley Scott', 'James Cameron',
-                   'Stanley Kubrick'],
-      'Actor': ['Al Pacino', 'Robert De Niro', 'Tom Hanks', 'Clint Eastwood', 'Mel Gibson', 'Quentin Tarantino',
-                'Leonardo DiCaprio', 'Morgan Freeman'],
-      'DocumentaryNarrator': ['Morgan Freeman', 'Tom Hanks', 'David Attenborough', 'James Earl Jones']}
 
 
 def invert_dict(dictionary):
@@ -24,12 +18,32 @@ def invert_dict(dictionary):
     return returned_dict
 
 
-print(invert_dict(stocks))
+#
+# with open("myfile.json", "w") as fp:
+#     json_string = json.dump(stocks, fp)
+#     print(json_string)
+#
+# pickle_file = open("myfile.txt", "w")
+# dict_to_string = pickle.dumps(stocks)
+#
+# load_string = pickle.loads(dict_to_string)
 
-# file1 = open("myfile.txt","w")
-import json
 
-with open("myfile.json", "w") as fp:
-    json.dump(stocks, fp)
+def write_dict(dictionary, file_name):
+    with open(file_name, 'wb') as handle:
+        pickle.dump(dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-# file= open()
+
+def read_dict(file_name):
+    with open(file_name, 'rb') as handle:
+        load_handle = pickle.load(handle)
+        print(load_handle)
+        return load_handle
+
+
+filename = 'stock_dict'
+i = read_dict(filename)
+inverted_stocks = invert_dict(stocks)
+write_dict(inverted_stocks, filename)
+
+# print(i == stocks)

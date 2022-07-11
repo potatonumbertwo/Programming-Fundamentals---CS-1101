934be7ee13d2b58efd7a5db41c981964d7c1a7ea
potatonumbertwo <lsj900605@gmail.com>
2022-07-10 20:50:22 -0400

    unit_4 remove spot words

--- cs_3308/indexer.py
+++ cs_3308/indexer.py
@@ -1,52 +1,44 @@
-#  Example code in python programming language demonstrating some of the features of an inverted index.
-#  In this example, we scan a directory containing the corpus of files. (In this case the numberOfDocuments are reports on articles
-#  and authors submitted to the Journal "Communications of the Association for Computing Machinery"  
-#
-#  In this example we see each currentFile being read, tokenized (each word or term is extracted) combined into a sorted
-#  list of unique terms.  
-#
-#  We also see the creation of a numberOfDocuments dictionary containing each document in sorted form with an index assigned to it.
-#  Each unique term is written out into a terms dictionary in sorted order with an index number assigned for each term.  
-#  From our readings we know that to complete teh inverted index fileNamesList that we need to do is create a third currentFile that will
-#  coorelate each term with the list of numberOfDocuments that it was extracted from.  We will do that in a later assignment.
-##
-#  We can further develop this example by keeping a reference for each term of the numberOfDocuments that it came from and by
-#  developing a list of the numberOfDocuments thus creating the term and document dictionaries.
-#
-#  As you work with this example, think about how you might enhance it to assign a unique index number to each term and to 
-#  each document and how you might create a documentContentsString structure that links the term index with the document index.
-
-
 import sys, os, re
 import time
+import datetime
+
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 # define global variables used as counters
-tokens = 0
-numberOfDocuments = 0
+# tokens = 0
+# numberOfDocuments = 0
 terms = 0
 termIndex = 0
 documentIndex = 0
 
 # initialize list variable
-#
 tokenList = []
 documentNameList = []
 
-#
 # Capture the start time of the routine so that we can determine the total running
 # time required to process the corpus
-#
-t2 = time.localtime()
+startTime = time.localtime()
+start = datetime.datetime.now()
 
 # set the name of the directory for the corpus
-#
-directoryToRead = "/Users/crowfordcindy/Desktop/git/cs_1101/cs_3308/cacm"
+# directoryToRead = "/Users/crowfordcindy/Desktop/git/cs_1101/cs_3308/cacm"
+directoryToRead = "/Users/crowfordcindy/Desktop/git/cs_1101/cs_3308/corpus"
+directoryToWrite = "/Users/crowfordcindy/Desktop/git/cs_1101/cs_3308"
+
 
 # For each document in the directory read the document into a string
-#
-fileNamesList = [fileName for fileName in os.listdir(directoryToRead)]
-for fileNameFromList in fileNamesList:
-    numberOfDocuments += 1
+
+
+def scan_directroy():
+    tokens = 0
+    numberOfDocuments = 0
+    fileNamesList = [fileName for fileName in os.listdir(directoryToRead)]
+    for fileNameFromList in fileNamesList:
+        numberOfDocuments += 1
     with open(directoryToRead + '/' + fileNameFromList, 'r') as currentFile:
         documentNameList.append(fileNameFromList)
         documentContentsString = currentFile.read().replace('\n', '')
@@ -54,46 +46,55 @@ for fileNameFromList in fileNamesList:
             tokenList.append(token)
             tokens += 1
 
+
+def remove_spot_words():
+    for i in range(1):
+        # this will convert
+        # the word into tokens
+        text_tokens = word_tokenize(tokens)
+
+    tokens_without_stop_words = [
+        word for word in text_tokens if not word in stopwords.words()]
+    # print(tokens_without_stop_words)
+
+
 # Open for write a currentFile for the document dictionary
-#
-fileToWrite = open(directoryToRead + '/' + 'numberOfDocuments.dat', 'w')
+fileToWrite = open(directoryToWrite + '/' + 'numberOfDocuments.dat', 'w')
 documentNameList.sort()
 for fileNameFromList in documentNameList:
     documentIndex += 1
     fileToWrite.write(fileNameFromList + ',' + str(documentIndex) + os.linesep)
 fileToWrite.close()
 
-#
 # Sort the tokens in the list
 tokenList.sort()
 
-#
-# Define a list for the unique terms  
-g = []
+# Define a list for the unique terms
+uniqueTermList = []
 
-#
 # Identify unique terms in the corpus
 for i in tokenList:
-    if i not in g:
-        g.append(i)
+    if i not in uniqueTermList:
+        uniqueTermList.append(i)
         terms += 1
 
-terms = len(g)
+terms = len(uniqueTermList)
 
 # Output Index to disk currentFile. As part of this process we assign an 'index' number to each unique term.
-# 
-indexfile = open(directoryToRead + '/' + 'index.dat', 'w')
-for i in g:
+indexFile = open(directoryToWrite + '/' + 'index.dat', 'w')
+for i in uniqueTermList:
     termIndex += 1
-    indexfile.write(i + ',' + str(termIndex) + os.linesep)
-indexfile.close()
+    indexFile.write(i + ',' + str(termIndex) + os.linesep)
+indexFile.close()
 
 # Print metrics on corpus
-#
-print('Processing Start Time: %.2d:%.2d' % (t2.tm_hour, t2.tm_min))
-print("Documents %i" % numberOfDocuments)
-print("Tokens %i" % tokens)
-print("Terms %i" % terms)
+print('Processing Start Time: %.2d:%.2d:%.2d' % (startTime.tm_hour, startTime.tm_min, startTime.tm_sec))
+print("The total number of documents processed: %i" % numberOfDocuments)
+print("The total number of terms: %i" % tokens)
+print("The total number of unique terms: %i" % terms)
+print("Total number of terms found that matched one of the stop words program’s stop words list: ")
 
-t2 = time.localtime()
-print('Processing End Time: %.2d:%.2d' % (t2.tm_hour, t2.tm_min))
+endTime = time.localtime()
+end = datetime.datetime.now()
+print('Processing End Time: %.2d:%.2d:%.2d' % (endTime.tm_hour, endTime.tm_min, endTime.tm_sec))
+print("Total running time: " + str((end - start)))
